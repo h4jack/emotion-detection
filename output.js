e10274c9fccccd7bf6cbc0a70c57ea375bd001ca
h4jack <susantamandi.dev@gmail.com>
2025-03-17 08:40:42 +0530

    added teh backup file and output file from deepface

---
+++ output.js
@@ -0,0 +1,23 @@
+const output = [
+    {
+        'emotion': {
+            'angry': np.float32(1.6120527),
+            'disgust': np.float32(9.067146e-09),
+            'fear': np.float32(0.0077687274),
+            'happy': np.float32(0.0026864698),
+            'sad': np.float32(0.60228163),
+            'surprise': np.float32(0.0030919574),
+            'neutral': np.float32(97.772125)
+        },
+        'dominant_emotion': 'neutral',
+        'region': {
+            'x': 262,
+            'y': 185,
+            'w': 197,
+            'h': 197,
+            'left_eye': (387, 258),
+            'right_eye': (316, 253)
+        },
+        'face_confidence': np.float64(0.94)
+    }
+]
